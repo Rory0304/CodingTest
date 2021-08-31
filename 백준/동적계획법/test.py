a1a81c4ce12847bcaf8db1bf2925fdf8082127cc
[2기-2반] 사은수 <mitty0304@naver.com>
2021-08-31 18:21:35 +0900

    2021/08/30 ~ 2021/08/31 algorithm update

---
+++ 백준/동적계획법/test.py
@@ -0,0 +1,18 @@
+MOD = 1_000_000_000
+
+n = int(input())
+stairs = [[0 for i in range(10)] for _ in range(101)]
+
+for i in range(1, 10):
+    stairs[1][i] = 1
+
+for i in range(2, n + 1):
+    for j in range(10):
+        if j == 0:
+            stairs[i][j] = stairs[i - 1][1]
+        elif j == 9:
+            stairs[i][j] = stairs[i - 1][8]
+        else:
+            stairs[i][j] = stairs[i - 1][j - 1] + stairs[i - 1][j + 1]
+            
+print(sum(stairs[n]) % MOD)
